eb54a756c64705a59c2fb20758187fad9ed245aa
BenPease21 <BAJA154@outlook.com>
2025-02-16 22:42:57 -0600

    Create StimulusSoup.cs

---
+++ StimulusSoup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    enum SoupType { Soup, Stew, Gumbo }
+    enum Ingredient { Mushrooms, Chicken, Carrots, Potatoes }
+    enum Seasoning { Spicy, Salty, Sweet }
+
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Simula's Soup Maker!\n");
+
+        Console.WriteLine("Choose a type: Soup, Stew, or Gumbo:");
+        string SoupInput = Console.ReadLine();
+        Console.WriteLine("Choose a main ingredient: Mushrooms, Chicken, Carrots, or Potatoes:");
+        string IngredientInput = Console.ReadLine();
+        Console.WriteLine("Choose a seasoning: Spicy, Salty, or Sweet:");
+        string SeasoningInput = Console.ReadLine();
+
+        var soup = ($"{SeasoningInput} {IngredientInput} {SoupInput} ");
+
+        Console.WriteLine($"You made: {soup}!");
+    }
+}
