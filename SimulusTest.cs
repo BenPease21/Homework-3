a7435264e2d7418cc892e62114a514e5131d0c1e
BenPease21 <BAJA154@outlook.com>
2025-02-16 22:02:55 -0600

    SimulusTest

---
+++ SimulusTest.cs
@@ -0,0 +1,45 @@
+﻿namespace OpenCloseLock_Chest
+{
+
+    enum ChestState
+    {
+        Locked,
+        Unlocked,
+        Open
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ChestState chest = ChestState.Locked;
+
+            while (true)
+            {
+                Console.WriteLine($"The chest is {chest.ToString()}. What do you want to do?");
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "unlock":
+                        if (chest == ChestState.Locked)
+                            chest = ChestState.Unlocked;
+                        break;
+                    case "lock":
+                        if (chest == ChestState.Unlocked)
+                            chest = ChestState.Locked;
+                        break;
+                    case "open":
+                        if (chest == ChestState.Unlocked)
+                            chest = ChestState.Open;
+                        break;
+                    case "close":
+                        if (chest == ChestState.Open)
+                            chest = ChestState.Unlocked;
+                        break;
+                }
+            }
+        }
+    }
+}
+
